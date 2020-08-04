d462a3de236c0f1f7303ebc7c1b00868df48e5b1
osotorrio <osotorrio@hotmail.com>
2020-08-04 10:48:50 +0100

    Check balance expression first draft

---
+++ csharp/Algorithms.Stacks/CheckBalancedExpression/CheckBalancedExpressionTest.cs
@@ -0,0 +1,46 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Algorithms.Stacks.CheckBalancedExpression
+{
+    public class CheckBalancedExpressionTest
+    {
+        [Theory]
+        [InlineData("()")]
+        [InlineData("{{}{}}")]
+        [InlineData("[]{}()")]
+        [InlineData("{[{}{}]}[()]")]
+        public void CheckReturnsTrue(string expression)
+        {
+            // Arrange
+            var checker = new ExpressionChecker();
+
+            // Act
+            var isBalanced = checker.IsBalanced(expression);
+
+            // Assert
+            isBalanced.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineData("{")]
+        [InlineData("]")]
+        [InlineData("][")]
+        [InlineData("])")] 
+        [InlineData("[(")]
+        [InlineData("[)")]
+        [InlineData("{(})")]
+        [InlineData("{()}[)")]
+        public void CheckReturnsFalse(string expression)
+        {
+            // Arrange
+            var checker = new ExpressionChecker();
+
+            // Act
+            var isBalanced = checker.IsBalanced(expression);
+
+            // Assert
+            isBalanced.ShouldBeFalse();
+        }
+    }
+}
---
+++ csharp/Algorithms.Stacks/CheckBalancedExpression/ExpressionChecker.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms.Stacks.CheckBalancedExpression
+{
+    public class ExpressionChecker
+    {
+        private readonly Stack _stack = new Stack();
+        private readonly List<char> _openChars = new List<char> { '(', '{', '['};
+        private readonly List<char> _closeChars = new List<char> { ')', '}', ']' };
+
+        public bool IsBalanced(string expression)
+        {
+            var arr = expression.ToCharArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var item = arr[i];
+
+                if (IsOpenChar(item))
+                {
+                    _stack.Push(item);
+                }
+
+                if (IsCloseChar(item))
+                {
+                    if (_stack.Count != 0)
+                    {
+                        var top = _stack.Pop();
+
+                        if (!AreComplementary((char)top, item))
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return _stack.Count == 0;
+        }
+
+        private bool AreComplementary(char open, char close)
+        {
+            if (open == '(')
+            {
+                return close == ')';
+            }
+
+            if (open == '{')
+            {
+                return close == '}';
+            }
+
+            if (open == '[')
+            {
+                return close == ']';
+            }
+
+            return false;
+        }
+
+        private bool IsOpenChar(char item)
+        {
+            return _openChars.Contains(item);
+        }
+
+        private bool IsCloseChar(char item)
+        {
+            return _closeChars.Contains(item);
+        }
+    }
+}
