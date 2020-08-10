77cff37a5ee5217c3720c57b73bc355173683a95
osotorrio <osotorrio@hotmail.com>
2020-08-10 11:07:27 +0100

    Stack implementation by two queues

---
+++ csharp/Algorithms.Queues/ImplementStack/StatckByTwoQueues.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Queues.ImplementStack
+{
+    public class StatckByTwoQueues<T>
+    {
+        private Queue<T> _mainQueue = new Queue<T>();
+
+        private Queue<T> _helperQueue = new Queue<T>();
+
+        public void Push(T newItem)
+        {
+            while (_mainQueue.Count != 0)
+            {
+                _helperQueue.Enqueue(_mainQueue.Dequeue());
+            }
+
+            _mainQueue.Enqueue(newItem);
+
+            while(_helperQueue.Count != 0)
+            {
+                _mainQueue.Enqueue(_helperQueue.Dequeue());
+            }
+        }
+
+        public T Pop()
+        {
+            return _mainQueue.Dequeue();
+        }
+    }
+}
